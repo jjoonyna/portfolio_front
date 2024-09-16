6337f2167fac2c5770cab1b375b280d32b8dad10
jjoonyna <nwj3808@gmail.com>
2024-09-16 18:29:51 +0900

    정보수정 완

--- src/Components/MyInfo.jsx
+++ src/Components/MyInfo.jsx
@@ -7,15 +7,18 @@ const MyInfo =()=>{
     const [error, setError] = useState('');
     const [userInfo, setUserInfo] = useState([]);
     const userId = sessionStorage.getItem('id');
-    
+    const [addr, setAddr] = useState('');
+    const [email, setEmail] = useState('');
+    const [license, setLicense] = useState('');
+
     useEffect(()=>{
         const GetUser = async()=>{
             try{
                 const response = await axios.get(`http://localhost:80/find_about/${userId}`);
                 setUserInfo(
-                    {id: 1, name: response.data.name, addr: response.data.addr,
-                    birth: response.data.birth, email: response.data.email, university: response.data.university,
-                    license: response.data.license
+                    {id: response.data.id, name: response.data.name, addr: setAddr(response.data.addr),
+                    birth: response.data.birth, email: setEmail(response.data.email), university: response.data.university,
+                    license: setLicense(response.data.license)
                     }
                 )
             }catch(error){
@@ -25,17 +28,35 @@ const MyInfo =()=>{
         GetUser();
     },[userId])
 
+
+    const UpdateInfo= async(event)=>{
+        event.preventDefault();
+        let formdata = {
+            id: userId, addr: addr, email: email, license: license
+        }
+        try{
+            const response = await axios.put(`http://localhost:80/update_about`,formdata);
+            setAddr(response.data.addr);
+            setEmail(response.data.email);
+            setLicense(response.data.license);
+        }catch(error){
+            alert("수정 실패");
+            setError("수저 실패",error.message);
+        }
+    }
+
     return(
         <div>
             <h1>내 정보 수정</h1>
             <div>
-                <form>
+                <form onSubmit={UpdateInfo}>
                     이름<input type="text" readOnly="readOnly" value={userInfo.name}/>
-                    주소<input type="text" value={userInfo.addr}/>
+                    주소<input type="text" value={addr} onChange={(e)=>setAddr(e.target.value)}/>
                     생일<input type="text" readOnly="readOnly" value={userInfo.birth}/>
-                    이메일<input type="text" value={userInfo.email}/>
+                    이메일<input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                     학력<input type="text" readOnly="readOnly" value={userInfo.university}/>
-                    자격증<input type="text" value={userInfo.license}/>
+                    자격증<input type="text" value={license} onChange={(e)=>setLicense(e.target.value)}/>
+                    <button type="submit">수정</button>
                 </form>
             </div>
         </div>
